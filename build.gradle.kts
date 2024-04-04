2988f07eb3d78cdcea97f86cb5f22d02109fab73
Ted Senft <code@robotgryphon.com>
2024-04-04 09:05:46 -0400

    Stop building nightlies on gradle file changes; fix errored root buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 tasks.create("mcVersion") {
     doFirst {
         val mc = libraries.versions.minecraft.get()
-        println($"version=$mc")
+        println("version=$mc")
     }
 }
 
 tasks.create("neoVersion") {
     doFirst {
         val neo = libraries.versions.neoforge.get()
-        println($"version=$neo")
+        println("version=$neo")
     }
 }
